3a3a191987ac3aabb02109e24c8cf6861d0e72d4
Matias <schkrk15@gmail.com>
2024-08-28 19:50:09 -0300

    Se modifico el js para apregar una api que muestre las peliculas

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
 
 document.getElementById('searchButton').addEventListener('click', searchMovies);
 
+let api_key = '9166d0b1ecff5365c910873ee49483b4';
+let urlBase = 'https://api.themoviedb.org/3/search/movie';
+let urlImg = 'https://image.tmdb.org/t/p/w200';
+
+
+let resultContainer = document.getElementById('results');
+
 function searchMovies() {
     let searchInput = document.getElementById('searchInput').value;
+    resultContainer.innerHTML = 'Cargando...'
+
+    fetch(`${urlBase}?api_key=${api_key}&query=${searchInput}`)
+    .then(respose => respose.json())
+    .then(response => displayMovies(response.results))
+}
+
+
+
+function displayMovies(movies) {
+
+    resultContainer.innerHTML = '';
+
+    if(movies.length === 0) {
+        resultContainer.innerHTML = '<p>No se encontraron resultado</p>';
+        return
+    } 
+
+    movies.forEach(movie => {
+        let movieDiv = document.createElement('div');
+        movieDiv.classList.add('movie');
+
+        let title = document.createElement('h2');
+        title.textContent = movie.title;
+
+        let releaseDate = document.createElement('p');
+        releaseDate.textContent = 'La fecha de lanzamiento fue: '+ movie.release_date;
+
+        let overview = document.createElement('p');
+        overview.textContent = movie.overview;
+
+        let posterPath = urlImg + movie.poster_path;
+        let poster = document.createElement('img');
+        poster.src = posterPath;
+
+
+        movieDiv.appendChild(poster);
+        movieDiv.appendChild(title);
+        movieDiv.appendChild(releaseDate);
+        movieDiv.appendChild(overview);
+
+        resultContainer.appendChild(movieDiv)
+    });
 
 }
